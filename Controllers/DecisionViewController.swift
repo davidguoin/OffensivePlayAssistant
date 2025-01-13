82b2f4ebabf06b803401e39cf3033745ead16ff9
David Guoin <davidguoin@gmail.com>
2025-01-12 19:29:49 -0500

    Initial Commit

---
+++ Controllers/DecisionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DecisionViewController.swift
+//  OffensivePlayAssistant
+//
+//  Created by Lori Lookliss on 1/24/25.
+//
+
+import UIKit
+
+class DecisionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Add your UI setup code here
+    }
+}
---
+++ Controllers/InputViewController.swift
@@ -0,0 +1,8 @@
+//
+//  InputViewController.swift
+//  OffensivePlayAssistant
+//
+//  Created by Lori Lookliss on 1/24/25.
+//
+
+import Foundation
